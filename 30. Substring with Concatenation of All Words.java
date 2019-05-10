0001002c74fcea627ceab1075ffb5305a2fab0ff
GarhomLee <garhomlee@gmail.com>
2019-05-09 23:10:29 -0400

    04/24/19

--- 30. Substring with Concatenation of All Words.java
+++ 30. Substring with Concatenation of All Words.java
@@ -1,8 +1,8 @@
 https://leetcode.com/problems/substring-with-concatenation-of-all-words/
 
-有两种解法，都是使用HashTable，解法二利用sliding window的思想进行进一步的优化。
+// 有两种解法，都是使用HashTable，解法二利用sliding window的思想进行进一步的优化。
 
-// 解法一：
+// 解法一：用HashTable，属于广义上的sliding window
 // 1）维护两个Map：第一个Map为记录words数组中出现的word及次数，key为word，value为出现的次数；第二个Map为遍历s时寻找到的word和出现的次数
 // 2）从0开始，到s.length() - wordsNum * wordLen【包含这个位置，即取等】，搜索是否出现words及出现次数是否相符
 // 3）维护临时变量num，表示当前遍历到的出现过的记录在allWords中的单词数量；同时维护临时变量start，表示每个单词的第一个字符位置，按wordLen更新
