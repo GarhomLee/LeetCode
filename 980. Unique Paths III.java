53b12ae5628e5be86cc0696c3f86cee247c80c5d
JIAHONG LI <jiahongli@jiahongs-mbp.dynamic.ucsd.edu>
2019-11-01 10:17:59 -0700

    10/31/19

--- 980. Unique Paths III.java
+++ 980. Unique Paths III.java
@@ -1,11 +1,17 @@
 https://leetcode.com/problems/unique-paths-iii/
 
-思路：Backtracking (DFS)
-
-时间复杂度：O(4^(rowLen * colLen))
-空间复杂度：O(rowLen * colLen)
-犯错点：1.细节错误：total记录的是所有0的个数，但到达2的步数是total+1。
-        2.细节错误：对所有choices搜索完后，要将状态复原，即grid[row][col]重置为0。
+// 思路：Backtracking (DFS)
+//         预处理：遍历二维数组grid，找到起始元素1的位置{startRow, startCol}和终止元素2的位置{endRow, endCol}，
+//             同时记录元素0的个数total。
+//             从元素1开始，进行dfs，直到找到元素2，且走过了所有的0元素（最终步数为total+1）。
+//         Goal: 找到元素2，且走过了所有的0元素（最终步数为total+1）。
+//         Choices:当前位置的4个相邻格子。对于经过的格子，标记为3，搜索完所有可能情况后重置为0。
+//         Constraints: 1）不能越界
+//                     2）不能重复走已经搜索过的格子
+// 时间复杂度：O(4^(rowLen * colLen))
+// 空间复杂度：O(rowLen * colLen)
+// 犯错点：1.细节错误：total记录的是所有0的个数，但到达2的步数是total+1。
+//         2.细节错误：对所有choices搜索完后，要将状态复原，即grid[row][col]重置为0。
 
 class Solution {
     int count = 0, total = 0;
